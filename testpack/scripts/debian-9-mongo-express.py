05072f09f28ac8ad272a18906dfd05dc4d64c50f
Brian Wilkinson <brian.wilkinson@fasthosts.com>
2018-02-05 17:35:02 +0000

    Use the new testpack helper library to provide common unittest functions

--- testpack/scripts/debian-9-mongo-express.py
+++ testpack/scripts/debian-9-mongo-express.py
@@ -4,54 +4,10 @@ import unittest
 import os
 import docker
 from selenium import webdriver
+from testpack_helper_library.unittests.dockertests import Test1and1Common
 
 
-class Test1and1MongoImage(unittest.TestCase):
-    docker_container = None
-    container_ip = None
-
-    @classmethod
-    def setUpClass(cls):
-        image_to_test = os.getenv("IMAGE_NAME")
-        if image_to_test == "":
-            raise Exception("I don't know what image to test")
-        client = docker.from_env()
-        Test1and1MongoImage.container = client.containers.run(
-            image=image_to_test,
-            remove=True,
-            detach=True,
-            network_mode="bridge",
-            user=10000,
-            ports={8080:8080},
-            working_dir="/var/www"
-        )
-
-        details = docker.APIClient().inspect_container(container=Test1and1MongoImage.container.id)
-        Test1and1MongoImage.container_ip = details['NetworkSettings']['IPAddress']
-
-    @classmethod
-    def tearDownClass(cls):
-        Test1and1MongoImage.container.stop()
-
-    def setUp(self):
-        print ("\nIn method", self._testMethodName)
-        self.container = Test1and1MongoImage.container
-
-    def execRun(self, command):
-        result = self.container.exec_run(command)
-        if isinstance(result, tuple):
-            exit_code = result[0]
-            output = result[1].decode('utf-8')
-        else:
-            output = result.decode('utf-8')
-        return output
-
-    def assertPackageIsInstalled(self, packageName):
-        op = self.execRun("dpkg -l %s" % packageName)
-        self.assertTrue(
-            op.find(packageName) > -1,
-            msg="%s package not installed" % packageName
-        )
+class Test1and1MongoImage(Test1and1Common):
 
     # <tests to run>
 
